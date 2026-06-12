21f755fc76874fcedec1f0677766a89a1dca6dfd
wijerett <winnettjeremy@gmail.com>
2026-06-12 10:03:54 -0400

    examples

--- time.js
+++ time.js
@@ -27,13 +27,26 @@
 
 /////////////////////////////////////////////////
 
-function sumObjectValues(obj) {
-    const copyObject = { ...obj };
-    let sum = 0;
-    Object.values(copyObject).forEach((value) => {
-        sum += value
-    });
-    return sum;
-}
+// function sumObjectValues(obj) {
+//     const copyObject = { ...obj };
+//     let sum = 0;
+//     Object.values(copyObject).forEach((value) => {
+//         sum += value
+//     });
+//     return sum;
+// }
 
-console.log(sumObjectValues({1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7}));
+// console.log(sumObjectValues({1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7}));
+
+//////////////////////////////////////////////////
+
+function get_sum(array) {
+    size = array.length;
+    if (size == 1) {
+        return array[0];
+    } else {
+        return (array[0] + get_sum(array.slice(1, size)))
+    }
+};
+
+console.log(get_sum([4, 5, 6]));
